96916838b4dea7fd3f51f941c5b33197f8e86aeb
Nina Rahimi <nina.i.rahimi@gmail.com>
2018-02-19 17:20:40 -0500

    Finally figured out how to check if a user puts in a negative number as their age

--- function_drills1.js
+++ function_drills1.js
@@ -1,7 +1,18 @@
 function whoAmI(name, age){
+    var yearOfBirth = 2018 - age;
     console.log(`Hi, my name is ${name} and I\'m ${age} years old`);
+    console.log("I was born in " + yearOfBirth);
+    
 }
 
 
+function howOld(number){
+   if(number < 0){
+      console.log("Not a valid entry");
+    }else{
+        console.log(number);
+    }
+}
 
+console.log(howOld(10));
 console.log(whoAmI("Sofia", "23"));
